c8d959d7f5f3deee7b9eebcaec5f0624eac7ff91
vince <vincent.harpin@orange.fr>
2023-01-14 17:07:43 +0100

    exercice 152

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
 // Premiere ligne
+$today = date("D");
+echo $today;
+echo "<br>";
 
+$today = date("2018.12.10");
+echo $today;
+echo "<br>";
 
-
-
-
-
+$today = date("11:35:07");
+echo $today;
